1a54b35bc486177176176ace37b8bab76ae0b68e
FedorProgrammer <59094110+FedorProgrammer@users.noreply.github.com>
2020-01-17 21:59:34 +0300

    Add files via upload

---
+++ oop_example.py
@@ -0,0 +1,49 @@
+class Shape:
+    def __init__(self, color, weight=0):
+        self.data = [1, 2, 3]
+        self.color = color
+        self.weight = weight
+
+    def get(self, i):
+        return self.data[i]
+
+    def square(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius, color='blue'):
+        super().__init__(color)
+        self.color = 'blue'
+        self.radius = radius
+
+    def square(self):
+        print('in circle ')
+        return 3.14 * self.radius ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, a, color='blue'):
+        super().__init__(color)
+        self.a = a
+
+    def square(self):
+        return self.a ** 2
+
+circle = Circle(3)
+circle.color = 'red'
+circle.weight = 10
+print(circle.square())
+circle.get(2)
+
+rect = Rectangle(2)
+print(rect.square())
+
+data = [circle, rect]
+
+
+def sum_of_squares(squares):
+    summa = 0
+    for figure in squares:
+        summa += figure.square()
+    return summa
