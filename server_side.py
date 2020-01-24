45995f3395180f3a73940d271a5cd219d07635d1
FedorProgrammer <59094110+FedorProgrammer@users.noreply.github.com>
2020-01-24 21:44:41 +0300

    Add files via upload

--- server_side.py
+++ server_side.py
@@ -2,40 +2,31 @@ import socket
 import threading
 
 HOST = 'localhost'
-PORT = 64231
+PORT = 12345
+
 
 def process_connection(sock, all_connections):
     while True:
         print(sock)
         print(addr)
-        data = sock.recv(1024)  # размер (байт) ждем пока напишут
+        data = sock.recv(1024)
         print(data)
-        decoded_data = data.decode('utf-8')  # декодируем
+        decoded_data = data.decode('utf-8')
         print(decoded_data)
-        # sock.sendall(data)  # эхо (отправляем сообщение себе)
-        # another_sock.sendall(data)  # отправляем сообщение клиенту (другому)
+        # sock.sendall(data)
+        # another_sock.sendall(data)
         for conn in all_connections:
-            conn.sendall(data)  # отправляем сообщение всем
+            conn.sendall(data)
 
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:  # создали object socket
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # теперь можем переиспользовать адрес
-    server.bind((HOST, PORT))  # поднимаем сервер
-    server.listen()  # сервер может читать
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen()
 
     connections = []
 
     while True:
-            connection, addr = server.accept()  # ждем пока кто-то снаружи постучится по адресу (возвращает соединение и адрес)
-            connections.append(connection)
-            threading.Thread(target=process_connection, args=(connection, connections)).start()
-        # another_connection, addr = server.accept()
-
-    #  while True:
-    #     print(addr)
-    #     data = connection.recv(1024)  # размер (байт) ждем пока напишут
-    #     print(data)
-    #     decoded_data = data.decode('utf-8')  # декодируем
-    #     print(decoded_data)
-    #     connection.sendall(data)  # эхо
+        connection, addr = server.accept()
+        connections.append(connection)
+        threading.Thread(target=process_connection, args=(connection, connections)).start()
